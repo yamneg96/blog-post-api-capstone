d7172d51c625d90c0cd6392e575417ed676aebc0
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-31 14:35:23 +0300

    Updated the delete from the single to all posts

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
@@ -7,12 +8,13 @@ class BlogPostListCreate(generics.ListCreateAPIView):
   queryset = BlogPost.objects.all() #Getting all the blog post models.
   serializer_class = BlogPostSerializer #Specifying the serializer class to convert the model instances to JSON.
 
+  def delete(self, request, *args, **kwargs):
+    BlogPost.objects.all().delete() # Delete all blog posts
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
 class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
   queryset = BlogPost.objects.all()
   serializer_class = BlogPostSerializer
   lookup_field = 'pk' #Specifying the field to look up the instance.
-
-  def delete(self, request, *args, **kwargs):
-    return super().delete(request, *args, **kwargs)
 # We use the django rest_framework views to use the generics.
 
