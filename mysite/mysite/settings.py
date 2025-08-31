9bb7152189ff6d593e8793280674f313362889cf
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-31 15:15:57 +0300

    Updated necessary files for deployment on vercel.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5oo8wwo%ow!_o%zfri3fm8#o88+$r6z3u-67^myt9jsqo63@fv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
