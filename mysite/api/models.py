55ef08e19688be764359f558ba0cef55cfaaccd9
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-31 13:02:39 +0300

    Created a database model

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-
-# Create your models here.
+# ORM maps python objects to database instances.
 class BlogPost(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  published_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+  def __str__(self):
+    return self.title
