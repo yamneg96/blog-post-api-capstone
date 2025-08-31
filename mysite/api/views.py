f8549dcae10f68b9c2ba608d13170a6e60e6e1dc
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-31 13:12:41 +0300

    Done creating views.

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer
 
-# Create your views here.
+class BlogPostListCreate(generics.ListCreateAPIView):
+  queryset = BlogPost.objects.all() #Getting all the blog post models.
+  serializer_class = BlogPostSerializer #Specifying the serializer class to convert the model instances to JSON.
+
+# We use the django rest_framework views to use the generics.
+
