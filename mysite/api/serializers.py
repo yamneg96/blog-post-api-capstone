217ef64c173890372db823d3acc2284f541a443f
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-31 13:09:51 +0300

    Done creating a serializer

---
+++ mysite/api/serializers.py
@@ -0,0 +1,9 @@
+# We specify a class that takes the model and converts it to a JSON format.
+from rest_framework import serializers
+from .models import BlogPost
+
+class BlogPostSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = BlogPost
+    flields = ['id', 'title', 'content', 'published_date']
+    
